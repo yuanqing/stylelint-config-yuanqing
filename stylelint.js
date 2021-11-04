a7da3b5c98707856386d33d3e4b16a7367fbbfcc
Yuan Qing Lim <desk@yuanqing.sg>
2021-11-04 20:22:06 +0800

    Use `stylelint-config-idiomatic-order`

--- stylelint.js
+++ stylelint.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [
     // See https://github.com/prettier/stylelint-config-prettier/blob/master/README.md#installation
     'stylelint-prettier/recommended',
-    // See https://github.com/constverum/stylelint-config-rational-order/blob/master/README.md#usage
-    'stylelint-config-rational-order'
+    // https://github.com/ream88/stylelint-config-idiomatic-order/blob/master/README.md#usage
+    'stylelint-config-idiomatic-order'
   ]
 }
